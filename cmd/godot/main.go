7f15a11db650e003d525e19b42f16fa26cb7cd3f
Denis Krivak <dokrivak@avito.ru>
2020-03-15 13:33:16 +0300

    Add filesystem traversal.

--- cmd/godot/main.go
+++ cmd/godot/main.go
@@ -6,6 +6,7 @@ import (
 	"go/parser"
 	"go/token"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/tetafro/godot"
@@ -20,23 +21,17 @@ func main() {
 	var files []*ast.File
 	fset := token.NewFileSet()
 
-	for _, f := range input {
-		stat, err := os.Stat(f)
-		if os.IsNotExist(err) {
-			fatal("File %s does not exist", f)
+	for _, path := range input {
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			fatal("Path %s does not exist", path)
 		}
-		if stat.IsDir() {
-			continue
+		for f := range findFiles(path) {
+			file, err := parser.ParseFile(fset, f, nil, parser.ParseComments)
+			if err != nil {
+				fatal("Failed to parse file %s: %v", path, err)
+			}
+			files = append(files, file)
 		}
-		if !strings.HasSuffix(f, ".go") {
-			continue
-		}
-
-		file, err := parser.ParseFile(fset, f, nil, parser.ParseComments)
-		if err != nil {
-			fatal("Failed to parse file %s: %v", f, err)
-		}
-		files = append(files, file)
 	}
 
 	for _, file := range files {
@@ -45,6 +40,26 @@ func main() {
 			fmt.Printf("%s: %s\n", msg.Message, msg.Pos)
 		}
 	}
+}
+
+func findFiles(root string) chan string {
+	out := make(chan string)
+
+	go func() {
+		filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+			sep := string(filepath.Separator)
+			if strings.HasPrefix(path, "vendor"+sep) || strings.Contains(path, sep+"vendor"+sep) {
+				return nil
+			}
+			if !info.IsDir() && strings.HasSuffix(info.Name(), ".go") {
+				out <- path
+			}
+			return nil
+		})
+		close(out)
+	}()
+
+	return out
 }
 
 func fatal(format string, args ...interface{}) {
