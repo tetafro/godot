8ce9eaff83227fa16862dd718b0e58ec1329668b
Denis Krivak <dokrivak@avito.ru>
2020-05-16 00:18:20 +0300

    Rework parsing flags.

--- cmd/godot/main.go
+++ cmd/godot/main.go
@@ -22,54 +22,83 @@ Options:
 	-h, --help      show this message
 	-v, --version   show version`
 
+type arguments struct {
+	help    bool
+	version bool
+	all     bool
+	files   []string
+}
+
 func main() {
-	if len(os.Args) < 2 {
-		fatalf(usage)
+	args, err := readArgs()
+	if err != nil {
+		fatalf("Error: %v", err)
 	}
-	if os.Args[1] == "-h" || os.Args[1] == "--help" {
+
+	if args.help {
 		fmt.Println(usage)
 		os.Exit(0)
 	}
-	if os.Args[1] == "-v" || os.Args[1] == "--version" {
+	if args.version {
 		fmt.Println(version)
 		os.Exit(0)
 	}
-	if strings.HasPrefix(os.Args[1], "-") && os.Args[1] != "-a" && os.Args[1] == "--all" {
-		fatalf("Unknown flag")
+	if len(args.files) == 0 {
+		fatalf(usage)
 	}
 
 	var settings godot.Settings
-	input := os.Args[1:]
-	if os.Args[1] == "-a" || os.Args[1] == "--all" {
-		if len(os.Args) < 3 {
-			fatalf(usage)
-		}
+	if args.all {
 		settings.CheckAll = true
-		input = os.Args[2:]
 	}
 
 	var files []*ast.File
 	fset := token.NewFileSet()
 
-	for _, path := range input {
+	for _, path := range args.files {
 		if _, err := os.Stat(path); os.IsNotExist(err) {
-			fatalf("Path %s does not exist", path)
+			fatalf("Path '%s' does not exist", path)
 		}
 		for f := range findFiles(path) {
 			file, err := parser.ParseFile(fset, f, nil, parser.ParseComments)
 			if err != nil {
-				fatalf("Failed to parse file %s: %v", path, err)
+				fatalf("Failed to parse file '%s': %v", path, err)
 			}
 			files = append(files, file)
 		}
 	}
 
-	for _, file := range files {
-		msgs := godot.Run(file, fset, settings)
-		for _, msg := range msgs {
-			fmt.Printf("%s: %s\n", msg.Message, msg.Pos)
+	for i := range files {
+		issues := godot.Run(files[i], fset, settings)
+		for _, iss := range issues {
+			fmt.Printf("%s: %s\n", iss.Message, iss.Pos)
 		}
 	}
+}
+
+func readArgs() (args arguments, err error) {
+	if len(os.Args) < 2 {
+		return arguments{}, fmt.Errorf("not enough arguments")
+	}
+
+	for _, arg := range os.Args[1:] {
+		if !strings.HasPrefix(arg, "-") {
+			args.files = append(args.files, arg)
+			continue
+		}
+
+		switch arg {
+		case "-h", "--help":
+			args.help = true
+		case "-v", "--version":
+			args.version = true
+		case "-a", "--all":
+			args.all = true
+		default:
+			return arguments{}, fmt.Errorf("unknown flag '%s'", arg)
+		}
+	}
+	return args, nil
 }
 
 func findFiles(root string) chan string {
